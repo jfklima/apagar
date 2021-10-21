add4ad44d55f5f714f7c7b4bc3152df1deb13fbb
jfklima <jfranklincontatos@gmail.com>
2021-10-21 10:33:58 -0300

    Retirado os comentários de avisos de tarefas.

--- lisp/manage-proj.el
+++ lisp/manage-proj.el
@@ -1,16 +1,5 @@
-;; instalar o magit
-;; disponibilizar uma chave para habilita-lo
 (use-package magit :ensure t
   :config
   (lmap "g s" 'magit-status))
-;; instalar o projectile
-;; definer o diretório onde irá procurar os projetos
-;; habilitar as chaves para:
-;;; explorar os arquivos do projeto
-;;; abriar um determinado projeto
-;; instalar o neotree
-;;; habilitar a chave para a abrir/fechar
-;;; habilitar a chave para abrir na raiz do projeto
-;;; aumentar a largura da janela
 
 (provide 'manage-proj)
