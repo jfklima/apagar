34ef960dd7bdbff9032edbcba765bd61cc2e45a5
jfklima <jfranklincontatos@gmail.com>
2021-10-21 14:16:52 -0300

    Instalado o color-theme spacemacs-theme.

--- lisp/ui.el
+++ lisp/ui.el
@@ -1,6 +1,13 @@
+;; usa o pacote de icones para melhorar a aparência
 (use-package all-the-icons :ensure t)
 
+;; usa o doom-modeline para não ter que se preocupar com linha de modo
 (use-package doom-modeline :ensure t
   :init (doom-modeline-mode 1))
+
+;; usa um thema de cor do spacemacs
+(use-package spacemacs-theme :ensure t
+  :defer t
+  :init (load-theme 'spacemacs-dark t))
 
 (provide 'ui)
