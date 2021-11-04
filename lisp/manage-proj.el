7c9c10177437f67a240a613869d3a98f03f6258f
jfklima <jfranklincontatos@gmail.com>
2021-11-04 09:33:01 -0300

    Instalado os pacotes projectile e o neotree.

--- lisp/manage-proj.el
+++ lisp/manage-proj.el
@@ -1,5 +1,28 @@
+(use-package projectile :ensure t
+  :init
+  (projectile-mode +1)
+  :bind
+  (:map projectile-mode-map
+	("M-p" . projectile-command-map))
+  :config
+  (setq projectile-project-search-path '("~/workspace/"))
+
+  (lmap
+    "p" '(:def nil :wk "project")
+    "p f" 'projectile-find-file
+    "p p" 'projectile-switch-project))
+
+(use-package neotree :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (lmap
+    "d" '(:def nil :wk "directory")
+    "d d" 'neotree-toggle))
+
 (use-package magit :ensure t
   :config
-  (lmap "g s" 'magit-status))
+  (lmap
+    "g" '(:def nil :wk "git")
+    "g s" 'magit-status))
 
 (provide 'manage-proj)
