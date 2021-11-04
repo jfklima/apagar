ad33ce99efd3d71006b91b4c6ec965dcb9d4fd01
jfklima <jfranklincontatos@gmail.com>
2021-11-04 09:34:48 -0300

    Instalado o pacote helm-dash para ser usado em python.

--- lisp/programming.el
+++ lisp/programming.el
@@ -1,5 +1,8 @@
-;; insere automaticamente o fechamento de parenteses, aspas entre outros
-(add-hook 'prog-mode-hook 'electric-pair-local-mode)
+(use-package helm-dash :ensure t
+  :config
+  (setq helm-dash-min-length 2))
+
+;; emacs-lisp
 
 (mmap
   :keymaps 'emacs-lisp-mode-map
@@ -7,5 +10,40 @@
 	 (progn
 	   (insert-parentheses)
 	   (multistate-insert-state 1))))
+
+;; python-mode
+
+(defun python-doc ()
+  (interactive)
+  (setq-local dash-docs-docsets '("Python 3")))
+
+(add-hook 'python-mode-hook 'python-doc)
+
+;; insere automaticamente o fechamento de parenteses, aspas entre outros
+(add-hook 'prog-mode-hook 'electric-pair-local-mode)
+
+(mmap
+  :keymaps 'python-mode-map
+  "s" '(:def helm-dash :wk "dash")
+  "c" '(:def python-shell-send-buffer :wk "shell-send-buffer")
+  "d" '(:def python-describe-at-point :wk "describe-at-point")
+  "f" '(:def python-eldoc-at-point :wk "eldoc-at-point")
+  "j" '(:def imenu :wk "imenu")
+  "l" '(:def python-shell-send-file :wk "shell-send-file")
+  "p" '(:def run-python :wk "run")
+  "r" '(:def python-shell-send-region :wk "shell-send-region")
+  "'" '(:def python-shell-send-string :wk)
+  "v" '(:def python-check :wk "check")
+  "z" '(:def python-shell-switch-to-shell :wk "shell-switch-to-shell")
+  "<" '(:def python-indent-shift-left :wk "indent-shift-left")
+  ">" '(:def python-indent-shift-right :wk "indent-shift-right")
+  "t" '(:def nil :wk "skeleton")
+  "t c" '(:def python-skeleton-class :wk "class")
+  "t d" '(:def python-skeleton-def :wk "def")
+  "t f" '(:def python-skeleton-for :wk "for")
+  "t i" '(:def python-skeleton-if :wk "if")
+  "t m" '(:def python-skeleton-import :wk "import")
+  "t t" '(:def python-skeleton-try :wk "try")
+  "t w" '(:def python-skeleton-while :wk "while"))
 
 (provide 'programming)
