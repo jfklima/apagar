8b86832ad041b1dce1489be0b266fad61bd883f4
jfklima <jfranklincontatos@gmail.com>
2021-11-04 15:24:09 -0300

    Removido todos os mapeamentos do mmap do python exceto do helm-dash

--- lisp/programming.el
+++ lisp/programming.el
@@ -43,26 +43,6 @@
 
 (mmap
   :keymaps 'python-mode-map
-  "s" '(:def helm-dash :wk "dash")
-  "c" '(:def python-shell-send-buffer :wk "shell-send-buffer")
-  "d" '(:def python-describe-at-point :wk "describe-at-point")
-  "f" '(:def python-eldoc-at-point :wk "eldoc-at-point")
-  "j" '(:def imenu :wk "imenu")
-  "l" '(:def python-shell-send-file :wk "shell-send-file")
-  "p" '(:def run-python :wk "run")
-  "r" '(:def python-shell-send-region :wk "shell-send-region")
-  "'" '(:def python-shell-send-string :wk)
-  "v" '(:def python-check :wk "check")
-  "z" '(:def python-shell-switch-to-shell :wk "shell-switch-to-shell")
-  "<" '(:def python-indent-shift-left :wk "indent-shift-left")
-  ">" '(:def python-indent-shift-right :wk "indent-shift-right")
-  "t" '(:def nil :wk "skeleton")
-  "t c" '(:def python-skeleton-class :wk "class")
-  "t d" '(:def python-skeleton-def :wk "def")
-  "t f" '(:def python-skeleton-for :wk "for")
-  "t i" '(:def python-skeleton-if :wk "if")
-  "t m" '(:def python-skeleton-import :wk "import")
-  "t t" '(:def python-skeleton-try :wk "try")
-  "t w" '(:def python-skeleton-while :wk "while"))
+  "s" '(:def helm-dash :wk "dash"))
 
 (provide 'programming)
