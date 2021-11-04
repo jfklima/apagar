20229d7d75d34043ceb00c390c3ba409b1c27d91
jfklima <jfranklincontatos@gmail.com>
2021-11-04 10:54:13 -0300

    Instalado o lsp-mode para trabalhar com Python

--- lisp/programming.el
+++ lisp/programming.el
@@ -1,3 +1,13 @@
+(use-package lsp-mode :ensure t
+  :init
+  (setq lsp-keymap-prefix "M-l")
+  :hook ((python-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :config
+  (setq lsp-idle-delay 0.500)
+  (setq lsp-log-io nil))
+
 (use-package helm-dash :ensure t
   :config
   (setq helm-dash-min-length 2))
@@ -18,6 +28,12 @@
   (setq-local dash-docs-docsets '("Python 3")))
 
 (add-hook 'python-mode-hook 'python-doc)
+
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
 
 ;; insere automaticamente o fechamento de parenteses, aspas entre outros
 (add-hook 'prog-mode-hook 'electric-pair-local-mode)
