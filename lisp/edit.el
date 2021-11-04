05e1ad930dbbda3d59cb2c1bbae54e52567fddce
jfklima <jfranklincontatos@gmail.com>
2021-11-04 10:21:17 -0300

    Restaurado o sistema de mapeamento padrão do company-mode

--- lisp/edit.el
+++ lisp/edit.el
@@ -22,12 +22,6 @@
   (setq company-idle-delay 0)
   (setq company-minimum-prefix-length 1)
   (setq company-selection-wrap-around t)
-
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-active-map (kbd "M-p") 'company-select-previous)
-  (define-key company-active-map (kbd "M-n") 'company-select-next)
-  (define-key company-active-map (kbd "<return>") 'company-abort)
   )
 
 (provide 'edit)
