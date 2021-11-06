bd51436e21fc784e9d53297a33225e30d1b53ab1
jfklima <jfranklincontatos@gmail.com>
2021-11-06 10:14:08 -0300

    Definido novas teclas de atalho para o neotree.

--- lisp/manage-proj.el
+++ lisp/manage-proj.el
@@ -5,16 +5,22 @@
   (:map projectile-mode-map
 	("M-p" . projectile-command-map))
   :config
-  (setq projectile-project-search-path '("~/workspace/"))
+  (setq projectile-project-search-path '("~/projetos/"))
 
   (lmap
     "p" '(:def nil :wk "project")
+    "p d" 'neotree-projectile-action
     "p f" 'projectile-find-file
-    "p p" 'projectile-switch-project))
+    "p o" 'projectile-switch-project))
 
 (use-package neotree :ensure t
   :config
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (define-key neotree-mode-map (kbd "r") 'neotree-rename-node)
+  (define-key neotree-mode-map (kbd "D") 'neotree-delete-node)
+  (define-key neotree-mode-map (kbd "a") 'neotree-create-node)
+  (define-key neotree-mode-map (kbd "c") 'neotree-copy-node)
+  (define-key neotree-mode-map (kbd "e") 'neotree-enter)
   (lmap
     "d" '(:def nil :wk "directory")
     "d d" 'neotree-toggle))
@@ -24,5 +30,10 @@
   (lmap
     "g" '(:def nil :wk "git")
     "g s" 'magit-status))
+
+(use-package all-the-icons-dired :ensure t
+  :hook (dired-mode . all-the-icons-dired-mode)
+  :config
+  (setq dired-del-marker t))
 
 (provide 'manage-proj)
