fcab79d24a045057504c3ba6179ddcfab4b74779
jfklima <jfranklincontatos@gmail.com>
2021-11-22 22:47:08 -0300

    Definido algumas teclas iniciais para navegar nos arquivos orgs

--- lisp/organize.el
+++ lisp/organize.el
@@ -1,5 +1,29 @@
+;; utilizar o org para monitorar as minhas tarefas de programação
+
 (use-package org
+  :hook
+  (org-mode . 'org-shifttab)
   :config
-  (setq org-hide-leading-stars t))
+  (setq org-hide-leading-stars t)
+  (mmap
+    :keymaps 'org-mode-map
+    "u" 'outline-up-heading
+    "l" 'org-insert-link
+    "o" 'org-open-at-point
+    "s" 'org-sparse-tree))
+
+;; criar um org template para org-capture
+;; criar um arquivo todo
+;; vincular uma teclar para o todo
+
+(defun todo ()
+  (interactive)
+  (find-file "~/org/todo.org"))
+
+(setq org-agenda-files '("~/org/notas.org"))
+
+(lmap
+  "o a" (lambda () (interactive) (org-agenda nil "a" nil))
+  "o o" (lambda () (interactive) (find-file "~/org/organizador.org")))
 
 (provide 'organize)
