2c16a9ed2b1367af13ab3a9586cf82f509aa7e6d
jfklima <jfranklincontatos@gmail.com>
2021-11-23 04:19:10 -0300

    Definido M-n para company-select-next in company-map.

--- lisp/edit.el
+++ lisp/edit.el
@@ -25,12 +25,11 @@
 
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  ;; (define-key company-active-map (kbd "M-n") 'company-select-next)
   (define-key company-active-map (kbd "M-p") 'company-select-previous)
+  (define-key company-active-map (kbd "M-n") 'company-select-next)
 
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
   (define-key company-active-map (kbd "<return>") 'company-abort)
-  (define-key company-active-map (kbd "M-n") 'company-complete-common-or-cycle)
   )
 
 (provide 'edit)
