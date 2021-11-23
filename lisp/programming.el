743d1386c5f268e09b712587e6074cee410d4e72
jfklima <jfranklincontatos@gmail.com>
2021-11-23 04:21:39 -0300

    Criado o mapeamento para insert-parenteses in emacs-lisp-map

--- lisp/programming.el
+++ lisp/programming.el
@@ -1,4 +1,11 @@
 ;; insere automaticamente o fechamento de parenteses, aspas entre outros
 (add-hook 'prog-mode-hook 'electric-pair-local-mode)
 
+(mmap
+  :keymaps 'emacs-lisp-mode-map
+  "i" '(lambda () (interactive)
+	 (progn
+	   (insert-parentheses)
+	   (multistate-insert-state 1))))
+
 (provide 'programming)
